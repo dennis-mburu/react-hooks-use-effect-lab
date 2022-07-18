7ce2f0042250808f1fddef6ca4a01dc820d80126
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-19 00:52:14 +0300

    Assignment Complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  
+  
+  function handleInterval(){
+    const intervalTimer = setInterval( () => {
+      setTimeRemaining((timeRemaining) => timeRemaining-1);
+    },1000)
+    return()=> clearInterval(intervalTimer)
+  }
 
+
+  function handleTimeOut(){
+
+    const timeOutTimer = setTimeout(
+      ()=> {setTimeRemaining(10)
+        onAnswered(false)
+      }
+      ,10000)
+
+    return () => clearTimeout(timeOutTimer)
+
+    
+  }
   // add useEffect code
+  useEffect(handleInterval, [])
+  useEffect(handleTimeOut, [])
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
 
   const { id, prompt, answers, correctIndex } = question;
 
